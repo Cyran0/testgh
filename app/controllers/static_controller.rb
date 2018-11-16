a2e586f3a401e2110f275cd32ad50fae6b71f310
Cyrano <bourgouinevan@gmail.com>
2018-11-16 04:59:18 +0100

    last commit front

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,10 @@
 class StaticController < ApplicationController
   def home
   end
+
+  def devs
+  end
+
+  def corpo
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  root 'static#home'
+  root to: 'static#home'
+
+  get '/home', to: 'static#home', as: 'home'
+
+  get '/devs', to: 'static#devs', as: 'devs'
+
+  get '/corpo', to: 'static#corpo', as: 'corpo'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
